4626e1a128444c0207125452c39f425f23203b4d
frequensy23-coder <mametevalex@gmail.com>
2023-02-27 22:33:06 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.squareup.retrofit2:retrofit:latest.release")
+    implementation("com.squareup.retrofit2:converter-jackson:latest.release")
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,39 @@
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.MediaType.Companion.toMediaType
+
+
+val client = OkHttpClient()
+val JSON = "application/json; charset=utf-8".toMediaType()
+
+
+fun translate(fromLang: String, toLang: String, text: String): String {
+    val requestBody = RequestBody.create(JSON,
+        """{
+"source_lang": "$fromLang",
+"target_lang": "$toLang",
+"mode": 0,
+"npage": 1,
+"source_text": "$text",
+"target_text": ""
+}""".trimIndent())
+    val request = Request.Builder()
+        .url("https://context.reverso.net/bst-query-service")
+        .addHeader("Origin", "https://context.reverso.net")
+        .addHeader("Accept-Language", "en-US,en;q=0.5")
+        .addHeader("X-Requested-With", "XMLHttpRequest")
+        .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:77.0) Gecko/20100101 Firefox/77.0")
+        .post(requestBody)
+        .build()
+    val response = client.newCall(request).execute()
+    return response.body!!.string()
+}
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}. Trs: ${translate("en", "ru", "square root")}")
+}
